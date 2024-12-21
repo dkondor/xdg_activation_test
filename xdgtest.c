9b7a3186872b823301d7ba259747bc123ebee0c7
Kondor Dániel <kondor.dani@gmail.com>
2024-12-21 22:48:55 +0100

    add more options to test activation

--- xdgtest.c
+++ xdgtest.c
@@ -20,27 +20,31 @@ int pipefd[2];
 GdkDisplay *dsp;
 GAppInfo *app;
 GtkSpinButton *timeout;
+FILE *f_pipe_write;
 
-static void token1(GtkButton*, void* data)
+static gboolean send_token(gpointer data)
 {
-	FILE *f = (FILE*)data;
+	char *id = (char*)data;
+	fprintf(f_pipe_write, "%s\n", id);
+	fflush(f_pipe_write);
+	g_free(id);
+	return FALSE; // do not repeat
+}
+
+static void token1(GtkButton*, void*)
+{
 	GdkAppLaunchContext *ctx = gdk_display_get_app_launch_context(dsp);
 	char *id = g_app_launch_context_get_startup_notify_id(G_APP_LAUNCH_CONTEXT(ctx), app, NULL);
 	fprintf(stderr, "[parent] got token: %s\n", id);
 	double delay = gtk_spin_button_get_value(timeout); // in seconds
 	if (delay > 0.0)
-	{
-		g_usleep((unsigned long)(1000000.0 * delay));
-	}
-	fprintf(f, "%s\n", id);
-	fflush(f);
-	g_free(id);
+		g_timeout_add((unsigned int)(1000.0 * delay), send_token, id);
+	else send_token(id);
 	g_object_unref(ctx);
 }
 
-static void token2(GtkButton*, void* data)
+static void token2(GtkButton*, void*)
 {
-	FILE *f = (FILE*)data;
 	GdkAppLaunchContext *ctx = gdk_display_get_app_launch_context(dsp);
 	char *id = g_app_launch_context_get_startup_notify_id(G_APP_LAUNCH_CONTEXT(ctx), app, NULL);
 	GdkAppLaunchContext *ctx2 = gdk_display_get_app_launch_context(dsp);
@@ -48,22 +52,17 @@ static void token2(GtkButton*, void* data)
 	fprintf(stderr, "[parent] got tokens: %s, %s\n", id, id2);
 	double delay = gtk_spin_button_get_value(timeout); // in seconds
 	if (delay > 0.0)
-	{
-		g_usleep((unsigned long)(1000000.0 * delay));
-	}
-	fprintf(f, "%s\n", id);
-	fflush(f);
-	g_free(id);
+		g_timeout_add((unsigned int)(1000.0 * delay), send_token, id);
+	else send_token(id);
 	g_free(id2);
 	g_object_unref(ctx);
 	g_object_unref(ctx2);
 }
 
-static void token0(GtkButton*, void* data)
+static void token0(GtkButton*, void*)
 {
-	FILE *f = (FILE*)data;
-	fprintf(f, "NO_TOKEN\n");
-	fflush(f);
+	fprintf(f_pipe_write, "NO_TOKEN\n");
+	fflush(f_pipe_write);
 }
 
 static gboolean child_read_pipe(GIOChannel *src, GIOCondition, gpointer data)
@@ -89,6 +88,29 @@ static gboolean child_read_pipe(GIOChannel *src, GIOCondition, gpointer data)
 	return TRUE;
 }
 
+static void dialog_activate(GtkButton*, void *data)
+{
+	GtkWindow *win = (GtkWindow*)data;
+	token1(NULL, NULL);
+	gtk_window_close(win);
+}
+
+static void dialog(GtkButton*, void *data)
+{
+	GtkWindow *parent = (GtkWindow*)data;
+	GtkWindow *win = GTK_WINDOW(gtk_window_new(GTK_WINDOW_TOPLEVEL));
+	gtk_window_set_title(win, "Dialog window");
+	gtk_window_set_transient_for(win, parent);
+	gtk_window_set_modal(win, TRUE);
+	
+	GtkBox *box = GTK_BOX(gtk_box_new(GTK_ORIENTATION_VERTICAL, 20));
+	GtkWidget *btn = gtk_button_new_with_label("Activate child");
+	gtk_box_pack_start(box, btn, TRUE, FALSE, 20);
+	g_signal_connect(btn, "clicked", G_CALLBACK(dialog_activate), win);
+	
+	gtk_container_add(GTK_CONTAINER(win), GTK_WIDGET(box));
+	gtk_widget_show_all(GTK_WIDGET(win));
+}
 
 int main(int argc, char **argv)
 {
@@ -101,7 +123,7 @@ int main(int argc, char **argv)
 	{
 		// parent process
 		close(pipefd[0]);
-		FILE *f = fdopen(pipefd[1], "w");
+		f_pipe_write = fdopen(pipefd[1], "w");
 		
 		gtk_init(&argc, &argv);
 		
@@ -118,10 +140,22 @@ int main(int argc, char **argv)
 		GtkWidget *btn1 = gtk_button_new_with_label("Valid token");
 		GtkWidget *btn2 = gtk_button_new_with_label("Expired token");
 		GtkWidget *btn3 = gtk_button_new_with_label("No token");
+		GtkWidget *btn4 = gtk_button_new_with_label("Show dialog");
 		GtkWidget *timeout2 = gtk_spin_button_new_with_range (0.0, 10.0, 0.1);
-		g_signal_connect(btn1, "clicked", G_CALLBACK(token1), f);
-		g_signal_connect(btn2, "clicked", G_CALLBACK(token2), f);
-		g_signal_connect(btn3, "clicked", G_CALLBACK(token0), f);
+		
+		GtkWidget *menubutton = gtk_menu_button_new();
+		gtk_button_set_label(GTK_BUTTON(menubutton), "Popup menu");
+		GtkWidget *menu = gtk_menu_new();
+		GtkWidget *item = gtk_menu_item_new_with_label("Activate child");
+		gtk_widget_show_all (item);
+		gtk_menu_shell_append(GTK_MENU_SHELL(menu), item);
+		gtk_menu_button_set_popup(GTK_MENU_BUTTON(menubutton), menu);
+		
+		g_signal_connect(btn1, "clicked", G_CALLBACK(token1), NULL);
+		g_signal_connect(btn2, "clicked", G_CALLBACK(token2), NULL);
+		g_signal_connect(btn3, "clicked", G_CALLBACK(token0), NULL);
+		g_signal_connect(btn4, "clicked", G_CALLBACK(dialog), win);
+		g_signal_connect(item, "activate", G_CALLBACK (token1), NULL);
 		g_signal_connect(win, "destroy", gtk_main_quit, NULL);
 		
 		GtkBox *box = GTK_BOX(gtk_box_new(GTK_ORIENTATION_VERTICAL, 10));
@@ -129,6 +163,8 @@ int main(int argc, char **argv)
 		gtk_box_pack_start(box, btn1, TRUE, FALSE, 5);
 		gtk_box_pack_start(box, btn2, TRUE, FALSE, 5);
 		gtk_box_pack_start(box, btn3, TRUE, FALSE, 5);
+		gtk_box_pack_start(box, btn4, TRUE, FALSE, 5);
+		gtk_box_pack_start(box, menubutton, TRUE, FALSE, 5);
 		gtk_box_pack_start(box, gtk_label_new("Delay before activating:"), TRUE, FALSE, 20);
 		gtk_box_pack_start(box, timeout2, TRUE, FALSE, 5);
 		timeout = GTK_SPIN_BUTTON(timeout2);
@@ -137,8 +173,7 @@ int main(int argc, char **argv)
 		gtk_widget_show_all(win);
 		
 		gtk_main();
-		fclose(f);
-		close(pipefd[1]);
+		fclose(f_pipe_write);
 		g_object_unref(app);
 	}
 	else
